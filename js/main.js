3eb98def3f37d5f7fc053b25700e26cebb237d0e
najmul57 <najmulcse247@gmail.com>
2024-10-28 10:36:23 +0600

    done

---
+++ js/main.js
@@ -0,0 +1,40 @@
+(function ($) {
+    "use strict";
+    
+
+// dark mode
+// const toggleButton = document.getElementById('toggleButton');
+// const body = document.body;
+// const header = document.querySelector('header');
+// const footer = document.querySelector('footer');
+
+// if (localStorage.getItem('darkMode') === 'enabled') {
+//     enableDarkMode();
+// }
+
+// toggleButton.addEventListener('click', function() {
+//     if (localStorage.getItem('darkMode') !== 'enabled') {
+//         enableDarkMode(); 
+//     } else {
+//         disableDarkMode(); 
+//     }
+// });
+
+// function enableDarkMode() {
+//     body.classList.add('dark-mode');
+//     header.classList.add('dark-mode');
+//     footer.classList.add('dark-mode');
+//     toggleButton.innerHTML = '<i class="fa-regular fa-sun"></i>'; // Icon for disabling dark mode
+//     localStorage.setItem('darkMode', 'enabled');
+// }
+
+// function disableDarkMode() {
+//     body.classList.remove('dark-mode');
+//     header.classList.remove('dark-mode');
+//     footer.classList.remove('dark-mode');
+//     toggleButton.innerHTML = '<i class="fa-regular fa-moon"></i>'; // Icon for enabling dark mode
+//     localStorage.setItem('darkMode', 'disabled'); 
+// }
+
+
+})(jQuery);
